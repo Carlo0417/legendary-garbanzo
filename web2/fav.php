170d508d99b51412eee1592daf9c01ec239ca567
RectoQueenie <201980119@psu.palawan.edu.ph>
2022-03-08 09:08:37 +0800

    web2 and web3

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,33 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-        
+
+        #First Name
+        if(!empty($_POST['username']))
+        {
+            $username = htmlentities($_POST['username']);
+            echo "Thank you for your selection " . $username . "<br>";
+        }
+
+        else { echo "First Name is EMPTY <br>"; } 
+
+        #Favorite Dish
+        if(!empty($_POST['dish']))
+        {
+            $dish = htmlentities($_POST['dish']);
+            echo "You really enjoyed " . $dish . "<br>";
+        } 
+
+        else { echo "Favorite Dish is EMPTY <br>"; } 
+
+        #Favorite Wine
+        if(!empty($_POST['color']))
+        {
+            $color = htmlentities($_POST['color']);
+            echo "- especially with a nice " . $color . " wine <br>";
+        } 
+
+        else { echo "Favorite Wine is EMPTY <br>"; }          
     ?>
 </body>
 </html>
--- web3/calc.php
+++ web3/calc.php
@@ -2,13 +2,38 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+       
+       $result = "";
+
         # test if input is_numeric
-        if(1){
+        if(!is_numeric($_POST['val1'] && $_POST['val2'])){
+            
+            $val1 = htmlentities($_POST['val1']);
+            $val2 = htmlentities($_POST['val2']);
+
             # test if calc is not null
-            if(1){
+            if(!is_null($_POST['calc'])){
+
+                $calc = htmlentities($_POST['calc']);
+
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($calc)
+                {
+                    case "add":
+                    $result = $val1 + $val2;
+                    break;
+
+                    case "sub":
+                    $result = $val1 - $val2;
+                    break;
+
+                    case "mul":
+                    $result = $val1 * $val2;
+                    break;
+
+                    case "div":
+                    $result = $val1 / $val2;
+                    break;        
                 }
                 echo "Calculation result: $result";
             }
